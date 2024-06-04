fab8e77c2fe20483bd43fa53a9f47bc8b369717f
HeadlessJohn <o_ayjh@naver.com>
2024-06-04 11:40:16 +0900

    FIRST

--- basys3_cpu_prj.srcs/sources_1/new/alu.v
+++ basys3_cpu_prj.srcs/sources_1/new/alu.v
@@ -1,23 +1,6 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 06/04/2024 11:17:46 AM
-// Design Name: 
-// Module Name: alu
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 
 module alu(
